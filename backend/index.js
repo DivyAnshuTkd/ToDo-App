58c62c43d55eb289da09b08c396768d818acfbd4
DivyAnshuTkd <divyanshu2907tkd@gmail.com>
2025-09-01 11:00:56 +0530

    September 1

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/todo", function(req, res)
+{
+
+})
+
+app.get("/todos", function(req, res)
+{
+
+})
+
+app.put("/completed", function(req, res)
+{
+
+})
+
+
+// Write basic express boilerplate code,
+// with express.json() middleware
