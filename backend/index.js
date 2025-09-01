b57782474541e649463c2b41b7932bc8f88bc15b
DivyAnshuTkd <divyanshu2907tkd@gmail.com>
2025-09-01 11:21:35 +0530

    Validate the schema of user input using zod library

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const { createTodo } = require("./types");
 const app = express();
 
 app.use(express.json());
 
 app.post("/todo", function(req, res)
 {
-
+    const createPayload = req.body;
+    const parsePayload = createTodo.safeParse(createPayload);
+    if(!parsePayload.success)
+    {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
 })
 
 app.get("/todos", function(req, res)
@@ -15,7 +24,16 @@ app.get("/todos", function(req, res)
 
 app.put("/completed", function(req, res)
 {
+    const updatePayload = req.body;
+    const parsePayload = createTodo.safeParse(updatePayload);
 
+    if(!parsePayload.success)
+    {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
 })
 
 
