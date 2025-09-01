f98442b964d3c611ccfe3f5db1ab5758b395a65a
DivyAnshuTkd <divyanshu2907tkd@gmail.com>
2025-09-01 13:07:30 +0530

    Data is successfully going into database

--- backend/db.js
+++ backend/db.js
@@ -10,7 +10,7 @@ const mongoose = require("mongoose");
 // mongodb url handy
 // mongodb+srv://ToDoApp_123:ToDoApp_123@cluster1.bwassnv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1
 
-mongoose.connect("mongodb+srv://ToDoApp_123:ToDoApp_123@cluster1.bwassnv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1")
+mongoose.connect("mongodb+srv://ToDoApp_123:ToDoApp_123@cluster1.bwassnv.mongodb.net/todos?retryWrites=true&w=majority&appName=Cluster1")
 
 const todoSchema = mongoose.Schema({
     title: String,
